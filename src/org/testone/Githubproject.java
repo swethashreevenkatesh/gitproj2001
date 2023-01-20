7074a8e345d4853a5691dd8cce89ffd534c50bc6
swethashreevenkatesh <swethav202@gmail.com>
2023-01-20 14:50:16 +0530

    testone

---
+++ src/org/testone/Githubproject.java
@@ -0,0 +1,15 @@
+package org.testone;
+
+public class Githubproject {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+
+	}
+	
+}
